460b86a3b195d7c8a904202a5cc998c43fc08aa3
Aadya Shrivastava <aadya17shrivastava@gmail.com>
2025-04-13 17:38:10 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const connectWallet = async () => {
+    if (window.ethereum) {
+      try {
+        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+        const walletAddress = accounts[0];
+        alert("Connected wallet: " + walletAddress);
+        // Redirect to portfolio form
+        window.location.href = "portfolio.html";
+      } catch (error) {
+        console.error("Connection error:", error);
+      }
+    } else {
+      alert("MetaMask not installed!");
+    }
+  };
+  
+  document.getElementById("connectWalletBtn")?.addEventListener("click", connectWallet);
+  
